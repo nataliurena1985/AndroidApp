4b98f4033889fd430f173dfc36cebcca628f544b
nataliurena1985 <72819120+nataliurena1985@users.noreply.github.com>
2023-08-21 14:07:15 -0300

    test button with findviewbyid

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.myapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtLabel : TextView = findViewById(R.id.txtLabel)
+        val btnChange : Button = findViewById(R.id.btnChange)
+
+        btnChange.setOnClickListener {
+            txtLabel.text = "Soy Claudia"
+        }
     }
 }
